1bfe6b5d50d8d7cd4ef890e5a4faec03489f7c4e
Adam Mika <mika.adam@gmail.com>
2013-09-20 21:46:21 -0500

    Generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digihack::Application.config.secret_token = '1df916b1b6364acbf0f7c44916ecf6dcbe61d6756241f1476f348d72dc55b388cc7f58e79fc3cb8e758c234910e063756e8f8e1bcf2e07e44761d89235664f6c'
