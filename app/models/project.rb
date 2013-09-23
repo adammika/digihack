56978773d3b1828a4e7fac2eb552d05c38967aba
Adam Mika <mika.adam@gmail.com>
2013-09-22 22:16:56 -0500

    Add memberships table

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+  has_many :memberships
+  has_many :users, :through => :memberships
+
   attr_accessible :description, :title
 end
